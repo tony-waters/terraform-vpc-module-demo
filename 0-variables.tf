11a93be28b44c28e86e8d629c58acb039453baaf
tony-waters <2tonywaters@gmail.com>
2025-09-07 00:36:12 +0100

    basic vpc module

---
+++ 0-variables.tf
@@ -0,0 +1,45 @@
+
+variable "azs" {
+  type        = list(string)
+  description = "Availability Zones"
+  default     = ["eu-west-2a"]
+  # default     = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+}
+
+variable "cidr_block" {
+  type = string
+  description = "CIDR block for VPN"
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "Public Subnet CIDR values"
+  default     = ["10.0.1.0/24"]
+  # default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "Private Subnet CIDR values"
+  default     = ["10.0.100.0/24"]
+  # default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "ec2_ami" {
+  type = string
+  description = "AMI to use for EC2 instances"
+  default = "ami-0cfb394ad3c3ac699"
+}
+
+variable "ec2_instance_type" {
+  type = string
+  description = "Instance Type to use for EC2 instances"
+  default = "t3.micro"
+}
+
+variable "ec2_key_pair_name" {
+  type = string
+  description = "AWS Key Pair to use with EC2"
+  default = "ec2_ssh"
+}
---
+++ 2-ec2.tf
@@ -0,0 +1,29 @@
+
+# module "ec2_instance" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
+#
+#   name = "single-instance"
+#
+#   instance_type = var.ec2_instance_type
+#   key_name      = var.ec2_key_pair_name
+#   monitoring    = true
+#   # subnet_id     =
+#
+#   tags = {
+#     Terraform   = "true"
+#     Environment = "dev"
+#   }
+# }
+#
+# resource "aws_instance" "public_ec2" {
+#   ami = var.ec2_ami
+#   instance_type = var.ec2_instance_type
+#   subnet_id = aws_subnet.public[0].id
+#   key_name = var.ec2_key_pair_name
+#   security_groups = [aws_security_group.ec2_public.id]
+#   associate_public_ip_address = true
+#
+#   tags = {
+#     Name = "EC2 in Public subnet 0"
+#   }
+# }
